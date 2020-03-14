a836b40bce3689b31303da4536dde45ff55df1c3
carit <carit@CarolaineJacqueline>
2020-03-14 00:38:01 -0300

    m

---
+++ WhilePorDoWhile/SoloPares2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SoloPares2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Ingrese Parametro");
+				int n = sc.nextInt();
+				int contador = 0;
+				int suma = 0;
+				do {
+					System.out.println(suma);
+					contador += 1;
+					suma += 2;
+				} while (contador < n);
+				{
+				}
+			}
