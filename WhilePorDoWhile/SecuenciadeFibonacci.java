5c358490e25e6490fa0067fd96ba695d94c5f4c6
carit <carit@CarolaineJacqueline>
2020-03-14 02:10:28 -0300

    secuencia

--- WhilePorDoWhile/SecuenciadeFibonacci.java
+++ WhilePorDoWhile/SecuenciadeFibonacci.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class SecuenciadeFibonacci {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese Parametro");
 
+		int n = sc.nextInt();
+		int num1 = 0;
+		int num2 = 1;
+		int fib;
+		int limite = 100;
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(num1);
+
+			fib = num1 + num2;
+			num1 = num2;
+			num2 = fib;
+		}
+
+	}
 }
